b962b0f6f5815537fb20ce69f06e852e5e9a6fb4
Tom Zhang <tommyawesome08@gmail.com>
2025-07-28 22:26:37 -0400

    admin list display

--- colors/admin.py
+++ colors/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Color
 
 
-admin.site.register(Color)
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = (
+        'hex_code',
+        'r',
+        'g',
+        'b',
+        'created_at',
+        'times_discovered',
+    )
